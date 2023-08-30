e667362fcf9cce140628bdc4d0eef0c9b613f863
Vivekkumar-cardbyte <vk0796221@gmail.com>
2023-08-30 22:58:11 +0530

    created chat-header component

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChatHeader } from "@/components/chat-header";
 import { Companion, Message } from "@prisma/client";
 
 interface ChatClientProps {
@@ -11,5 +12,7 @@ interface ChatClientProps {
   };
 }
 export const ChatClient = ({companion}:ChatClientProps) => {
-  return <div>news</div>;
+  return <div className="flex flex-col h-full p-4 space-y-2">
+    <ChatHeader companion={companion}/>
+  </div>;
 };
